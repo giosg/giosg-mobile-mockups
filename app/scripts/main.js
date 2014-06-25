f8b1d0339ad0bc0107b882bade4204c7cbd87e94
Kimmo Kiiski <kimmo.kiiski@witchcraftlabs.com>
2014-06-25 10:43:38 +0300

    Clicking the visitor button activates the attention state

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,25 @@
-console.log('\'Allo \'Allo!');
+$(function() {
+    'use strict';
+    var visitorInterval = null;
+    var counter;
+    var isActive = false;
+    $('body').on('click', '#visitor-button', function() {
+        var $this = $(this);
+        var $badge = $this.find('.btn-badge');
+        if (isActive) {
+            isActive = false;
+            clearInterval(visitorInterval);
+            $this.removeClass('btn-attention');
+            $badge.hide();
+        } else {
+            isActive = true;
+            counter = 0;
+            $badge.text('0').show();
+            $this.addClass('btn-attention');
+            visitorInterval = setInterval(function() {
+                counter += 1;
+                $badge.text('' + counter);
+            }, 1000);
+        }
+    });
+});
